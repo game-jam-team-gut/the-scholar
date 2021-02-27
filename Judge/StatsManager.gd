bb5200155dce7dc64d6f772734a20e5b1d1a3596
filip-szweda <filip.szweda1@gmail.com>
2021-02-27 10:30:05 +0100

    Change in variable names, so they suit the lore. More buttons. Empty credits scene.

--- Judge/StatsManager.gd
+++ Judge/StatsManager.gd
@@ -1,11 +1,11 @@
 extends Node
 
 
-var current_secret_knowledge
-export var max_secret_knowledge = 100
+var current_mythical_knowledge
+export var max_mythical_knowledge = 100
 
-var current_life
-export var max_life = 100
+var current_health
+export var max_health = 100
 
 var current_sanity
 export var max_sanity = 100
@@ -13,8 +13,8 @@ export var max_sanity = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	current_secret_knowledge = max_secret_knowledge
-	current_life = max_life
+	current_mythical_knowledge = max_mythical_knowledge
+	current_health = max_health
 	current_sanity = max_sanity
 
 

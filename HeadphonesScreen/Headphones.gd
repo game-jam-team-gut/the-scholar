385789af0ac216a98e84a502e8901ff6bccbe36c
Mateusz Grzonka <alpinus4@gmail.com>
2021-02-28 16:54:54 +0100

    Add headphones recommended screen

---
+++ HeadphonesScreen/Headphones.gd
@@ -0,0 +1,33 @@
+extends Control
+
+
+onready var alpha_tween = get_node("Tween")
+
+func _ready():
+	self.modulate.a = 0
+	yield(get_tree().create_timer(0.5), "timeout")
+	alpha_tween.interpolate_property(
+			self, 
+			"modulate", 
+			modulate, 
+			Color(modulate.r, modulate.g, modulate.b, 1), 
+			1, 
+			Tween.TRANS_CUBIC
+			)
+	alpha_tween.start()
+	yield(get_tree().create_timer(3), "timeout")
+	alpha_tween.interpolate_property(
+			self, 
+			"modulate", 
+			modulate, 
+			Color(modulate.r, modulate.g, modulate.b, 0), 
+			1, 
+			Tween.TRANS_CUBIC
+			)
+	alpha_tween.start()
+	yield(get_tree().create_timer(1), "timeout")
+	get_tree().change_scene("res://MainMenu.tscn")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
